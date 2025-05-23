b233cb27c9be1500ca92158030e2270458ed1b5c
theogravity <theo@airtop.ai>
2025-05-23 14:06:18 -0700

    fix(loglayer): fixes import issues caused by exporting LogLevel from multiple packages

--- packages/core/loglayer/src/index.ts
+++ packages/core/loglayer/src/index.ts
@@ -2,8 +2,26 @@ export { LogLayer } from "./LogLayer.js";
 export { MockLogLayer } from "./MockLogLayer.js";
 export { MockLogBuilder } from "./MockLogBuilder.js";
 export * from "./types/index.js";
-export type { ErrorOnlyOpts, ILogLayer, ILogBuilder } from "@loglayer/shared";
 export { ConsoleTransport } from "./transports/ConsoleTransport.js";
 export { TestLoggingLibrary } from "./TestLoggingLibrary.js";
 export { TestTransport } from "./transports/TestTransport.js";
-export * from "@loglayer/plugin";
+
+export type {
+  PluginBeforeDataOutFn,
+  PluginShouldSendToLoggerFn,
+  PluginBeforeMessageOutFn,
+  PluginOnMetadataCalledFn,
+  PluginOnContextCalledFn,
+} from "@loglayer/plugin";
+
+export { PluginCallbackType } from "@loglayer/plugin";
+
+export type {
+  LogLevelType,
+  ErrorOnlyOpts,
+  ILogLayer,
+  ILogBuilder,
+  LogLayerTransport,
+} from "@loglayer/shared";
+
+export { LogLevel } from "@loglayer/shared";
